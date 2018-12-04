c5542f539fa3c8e1be24a0a3a31785802598bd3e
KYDYRBAEVA <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-04 14:36:05 +0600

    Update jquery.faded.js

--- foto art/js/jquery.faded.js
+++ foto art/js/jquery.faded.js
@@ -15,6 +15,11 @@ if(typeof jQuery != "undefined") {
 						if (o.random) {
 							$.fn.reorder = function(callback) {
 								function randOrd() { return(Math.round(Math.random())-0.5); }
+									return($(this).each(function() {
+									var $this = $(this);
+									var $children = $this.children();
+									var childCount = $children.length;
+									if (childCount > 1) {
 										$children.hide();
 										var indices = new Array();
 										for (i=0;i<childCount;i++) { indices[indices.length] = i; }
@@ -22,57 +27,3 @@ if(typeof jQuery != "undefined") {
 										$.each(indices,function(j,k) { 
 											var $child = $children.eq(k);
 											var $clone = $child.clone(true);
-											$clone.show().appendTo($this);
-											if (callback !== undefined) {
-												callback($child, $clone);
-											}
-										$child.remove();
-									});
-									}
-								}));
-							};
-							$c.reorder();
-						}
-						function pause() {
-							clearInterval(autoplay);
-							clearTimeout(restart);
-							restart = setTimeout(function() {
-								autoplay = setInterval(function(){
-									animate("next");
-								},o.autoplay);
-							},o.autorestart);			
-						}				
-						if (o.autoheight) {
-							$c.animate({height: $c.children(":eq(0)").outerHeight()},o.autoheight);
-						}
-						if (o.pagination) {
-							if (o.autopagination) {
-								$t.append("<ul class="+o.pagination+"></ul>");
-								$c.children().each(function(){
-									$("."+o.pagination+"",$t).append("<li><a rel="+number+" href=\"#\" >"+(number+1)+"</a></li>");
-									number++;
-								});
-							}
-							$("."+o.pagination+" li a:eq(0)",$t).parent().addClass("current");
-							$("."+o.pagination+" li a",$t).click(function(){
-								current = $("."+o.pagination+" li.current a",$t).attr("rel");									
-								clicked = $(this).attr("rel");
-								if (current != clicked) {animate("pagination",clicked,current);}
-								if(o.autoplay){pause();}
-								return false;
-							});
-						}
-						if (o.sequentialloading&&$c.children()[0].tagName=="IMG") {
-							$c.css({background:"url("+o.loadingimg+") no-repeat 50% 50%"});
-							imgSrc = $("img:eq(0)",$c).attr("src");
-							$("img:eq(0)",$c).attr("src", imgSrc).load(function() {
-								$(this).fadeIn(o.speed,function(){
-									loaded = true;
-								});
-							});
-						} else {
-							$c.find(":eq(0)").fadeIn(o.speed,function(){
-								loaded = true;
-							});
-						}
-						
