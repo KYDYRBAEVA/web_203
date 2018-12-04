0fc626822dee40ef0cf86286d00a04732ca7d03a
KYDYRBAEVA <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-04 14:03:32 +0600

    Update jquery.faded.js

--- foto art/js/jquery.faded.js
+++ foto art/js/jquery.faded.js
@@ -15,11 +15,6 @@ if(typeof jQuery != "undefined") {
 						if (o.random) {
 							$.fn.reorder = function(callback) {
 								function randOrd() { return(Math.round(Math.random())-0.5); }
-									return($(this).each(function() {
-									var $this = $(this);
-									var $children = $this.children();
-									var childCount = $children.length;
-									if (childCount > 1) {
 										$children.hide();
 										var indices = new Array();
 										for (i=0;i<childCount;i++) { indices[indices.length] = i; }
@@ -47,14 +42,6 @@ if(typeof jQuery != "undefined") {
 								},o.autoplay);
 							},o.autorestart);			
 						}				
-						$c.css({position:"relative"});			
-						$c.children().css({
-							position:"absolute",
-							top: 0, 
-							left: 0,
-							zIndex: 0,
-							display:"none"
-						 });
 						if (o.autoheight) {
 							$c.animate({height: $c.children(":eq(0)").outerHeight()},o.autoheight);
 						}
