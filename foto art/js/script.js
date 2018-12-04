e3eddf6dac9159535ac99abee79b9e2095da080c
KYDYRBAEVA <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-04 13:35:46 +0600

    Update script.js

--- foto art/js/script.js
+++ foto art/js/script.js
@@ -17,4 +17,16 @@ document.addEventListener( 'DOMContentLoaded', function() {
 	try { $('body').unmousewheel(); } catch( error ) {}
 });
 
+$(document).ready(function(){
+					  
+ $("a.new_window").attr("target", "_blank");
+  
+ $(".faded").faded({
+	speed: 500,
+	crossfade: true,
+	autopagination:false
+  });
+  
+});
+
 
