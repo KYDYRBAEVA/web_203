7832fa7980e5378f50fc2f7bd52fddc962d71b13
KYDYRBAEVA <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-06 14:27:16 +0600

    Update cufon-replace.js

--- foto art/js/cufon-replace.js
+++ foto art/js/cufon-replace.js
@@ -1,1 +1,2 @@
 Cufon.replace('h1', { fontFamily: 'AG Foreigner Light-Plain', color: '-linear-gradient(#aeaeae, #8c8c8c)' });
+Cufon.replace('h1 span',{ fontFamily: 'AG Foreigner Light-Plain', color: '-linear-gradient(#dcdcdc, #b2b2b2)'});
