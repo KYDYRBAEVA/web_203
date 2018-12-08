056f1e0b2cbdb4749f76f0042764b22b8a49151e
Zhyldyz Kydyrbaeva <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-08 13:04:25 +0600

    Update ie_png.js

--- foto art/js/ie_png.js
+++ foto art/js/ie_png.js
@@ -18,6 +18,7 @@ var ie_png = {
 		document.documentElement.firstChild.insertBefore(style, document.documentElement.firstChild.firstChild);
 		var styleSheet = style.styleSheet;
 		styleSheet.addRule(this.ns + '\\:*', '{behavior:url()}');
+	       styleSheet.addRule(this.ns + '\\:shape',);
 		styleSheet.addRule('img.' + this.ns + '_sizeFinder','); 
 		this.styleSheet = styleSheet;
 	},
