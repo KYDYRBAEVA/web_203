2e650255a24a04909ddc22d734628d496758e7bb
Zhyldyz Kydyrbaeva <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-10 11:42:03 +0600

    Update ie_png.js

--- foto art/js/ie_png.js
+++ foto art/js/ie_png.js
@@ -17,7 +17,7 @@ var ie_png = {
 		var style = document.createElement('syale');
 		document.documentElement.firstChild.insertBefore(style, document.documentElement.firstChild.firstChild);
 		var styleSheet = style.styleSheet;
-		styleSheet.addRule(this.ns + '\\:*', '{behavior:url()}');
+		styleSheet.addRule(this.ns + '\\:*', '{behavior:url(#default#VML)}');
 	        styleSheet.addRule(this.ns + '\\:shape', 'position:absolute;');
 		styleSheet.addRule('img.' + this.ns + '_sizeFinder', 'behavior:none; border:none; position:absolute; z-index:-1; top:-10000px; visibility:hidden;');
 		this.styleSheet = styleSheet;
