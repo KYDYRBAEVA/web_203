5710490efd39dc2b89be48819a5a647f2fbf6bce
Zhyldyz Kydyrbaeva <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-10 12:03:12 +0600

    Update ie_png.js

--- foto art/js/ie_png.js
+++ foto art/js/ie_png.js
@@ -1,4 +1,19 @@
+var ie_png = {
 
+	ns: 'ie_png',
+	imgSize: {},
+	
+	createVmlNameSpace: function() { 
+		if (document.namespaces && !document.namespaces[this.ns]) {
+		  document.namespaces.add(this.ns, 'urn:schemas-microsoft-com:vml');
+		}
+		if (window.attachEvent) {
+			window.attachEvent('onbeforeunload', function() {
+				ie_png = null;
+			});
+		}
+	},
+	
 			
 	
 	
