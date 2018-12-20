2cd21491a03697f01e65c6269d3d448195fb8e1c
Zhyldyz Kydyrbaeva <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-12-19 21:58:18 -1200

    Add files via upload

---
+++ foto art(new)/js/script.js
@@ -0,0 +1,32 @@
+// Smooth scroll blocking
+document.addEventListener( 'DOMContentLoaded', function() {
+	if ( 'onwheel' in document ) {
+		window.onwheel = function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'DOMMouseScroll', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	} else if ( 'onmousewheel' in document ) {
+		window.onmousewheel= function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'onmousewheel', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	}
+
+	try { $('body').unmousewheel(); } catch( error ) {}
+});
+
+$(document).ready(function(){
+					  
+  $("a.new_window").attr("target", "_blank");
+  
+  $(".faded").faded({
+		speed: 500,
+		crossfade: true,
+		autopagination:false
+  });
+  
+});
